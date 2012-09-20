e5a0019cc2d3b68f0d7fbd3614deba9abad34293
Trevor Kimenye <kimenye@gmail.com>
2012-09-20 18:25:34 +0300

    Started working on the ui test

--- public/javascripts/game/game-ui.js
+++ public/javascripts/game/game-ui.js
@@ -1,7 +1,33 @@
+var BackgroundUI = JS.Class({
+    construct : function(imagePath,targetLayer,x,y) {
+        var _self = this;
+
+        var _bgImageObj = new Image();
+        _bgImageObj.onload = function() {
+            var _img = new Kinetic.Image({
+                x: isSomethingMeaningful(x) ? x : 0,
+                y: isSomethingMeaningful(y) ? y : 0,
+                image: _bgImageObj
+            });
+
+            targetLayer.add(_img);
+            _img.moveToBottom();
+            targetLayer.draw();
+
+            console.log("Loaded splash screen. Z-index ", _img.getZIndex());
+//            SHOTGUN.fire(Constants.EVENT_READY, ["splash","Splash screen loaded"],'splash');
+        };
+        _bgImageObj.src = imagePath;
+    }
+});
+
 var GamePlayerUI = JS.Class({
     construct : function(id, name) {
         this.id = id;
         this.name = name;
+    },
+    display: function() {
+
     }
 });
 
@@ -24,5 +50,10 @@ var GameUI = JS.Class({
             width: GameUI.width,
             height: GameUI.height
         });
+
+        this.backgroundLayer = new Kinetic.Layer();
+
+        this.bg = new BackgroundUI("images/woodback.jpg", this.backgroundLayer, 0, 0);
+        this.stage.add(this.backgroundLayer);
     }
 });
