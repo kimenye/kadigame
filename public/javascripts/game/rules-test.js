93d1a63ce2210c475f1db54d2bd0b479011b7034
Trevor Kimenye <kimenye@gmail.com>
2012-10-01 19:31:10 +0300

    Added rules logic

--- public/javascripts/game/rules-test.js
+++ public/javascripts/game/rules-test.js
@@ -18,20 +18,20 @@ describe("Card rules:", function() {
     var rule = new kadi.game.RuleEngine();
 
     it("Only valid cards can start", function() {
-        var king = new kadi.game.Card(kadi.game.Card.KING,kadi.game.Suite.DIAMONDS);
+        var king = diamonds("K");
         expect(kadi.game.RuleEngine.canStart(king)).toBe(false);
 
-        var queen = new kadi.game.Card(kadi.game.Card.JACK, kadi.game.Suite.HEARTS);
+        var queen = hearts("Q");
         expect(kadi.game.RuleEngine.canStart(queen)).toBe(false);
 
-        var five = new kadi.game.Card(kadi.game.Card.FIVE, kadi.game.Suite.HEARTS);
+        var five = hearts("5");
         expect(kadi.game.RuleEngine.canStart(five)).toBe(true);
     });
 
     it("Cards of the same suite can follow each other", function() {
-        var random_spade_card = new kadi.game.Card("6",kadi.game.Suite.SPADES);
-        var other_spade_card = new kadi.game.Card("5",kadi.game.Suite.SPADES);
-        var cant_follow = new kadi.game.Card("4",kadi.game.Suite.DIAMONDS);
+        var random_spade_card = spades("6");
+        var other_spade_card = spades("5");
+        var cant_follow = diamonds("4");
 
         expect(kadi.game.RuleEngine.canFollow(random_spade_card, other_spade_card)).toBe(true);
         expect(kadi.game.RuleEngine.canFollow(other_spade_card,random_spade_card)).toBe(true);
@@ -40,56 +40,56 @@ describe("Card rules:", function() {
 
     it("Cards of a different suite but same rank can follow each other", function() {
         var rank = "4";
-        var random_diamond = new kadi.game.Card(rank,kadi.game.Suite.DIAMONDS);
-        var random_spade = new kadi.game.Card(rank,kadi.game.Suite.DIAMONDS);
+        var random_diamond = diamonds(rank);
+        var random_spade = spades(rank);
 
         expect(kadi.game.RuleEngine.canFollow(random_diamond,random_spade)).toBe(true);
     });
 
     it("An ace can follow any card", function() {
-        var any_card = new kadi.game.Card("Q", kadi.game.Suite.DIAMONDS );
-        var an_ace = new kadi.game.Card("A", kadi.game.Suite.SPADES);
+        var any_card = diamonds("Q");
+        var an_ace = spades("A");
 
         expect(kadi.game.RuleEngine.canFollow(an_ace,any_card));
     });
 
     it("A card can be be in a move with another if they are the same rank", function() {
-        var five_d = new kadi.game.Card("5", kadi.game.Suite.DIAMONDS);
-        var five_s = new kadi.game.Card("5", kadi.game.Suite.SPADES);
+        var five_d = diamonds("5");
+        var five_s = spades("5");
 
         expect(kadi.game.RuleEngine.canPlayTogetherWith(five_d, five_s)).toBe(true);
     });
 
     it("A card cannot be in a move with another if they are not the same rank", function() {
-        var five_d = new kadi.game.Card("5", kadi.game.Suite.DIAMONDS);
-        var six_d = new kadi.game.Card("6", kadi.game.Suite.DIAMONDS);
+        var five_d = diamonds("5");
+        var six_d = diamonds("6");
 
         expect(kadi.game.RuleEngine.canPlayTogetherWith(five_d, six_d)).toBe(false);
     });
 
     it("An ace can not be in a move a non ace card", function() {
-        var five_d = new kadi.game.Card("5", kadi.game.Suite.DIAMONDS);
-        var ace = new kadi.game.Card("A", kadi.game.Suite.DIAMONDS);
+        var five_d = diamonds("5");
+        var ace = diamonds("A");
 
         expect(kadi.game.RuleEngine.canPlayTogetherWith(five_d, ace)).toBe(false);
     });
 
     it("A queen cannot finish a move", function() {
-        var card = new kadi.game.Card("Q", kadi.game.Suite.DIAMONDS);
+        var card = diamonds("Q");
         expect(kadi.game.RuleEngine.canEndMove(card)).toBe(false);
 
-        card = new kadi.game.Card("K", kadi.game.Suite.DIAMONDS);
+        card = diamonds("K");
         expect(kadi.game.RuleEngine.canEndMove(card)).toBe(true);
 
-        card = new kadi.game.Card("Q", kadi.game.Suite.DIAMONDS);
+        card = diamonds("Q");
         expect(kadi.game.RuleEngine.canEndMove(card)).toBe(false);
     });
 
     describe("Queen rules", function() {
 
         it("A queen can play together with another queen", function() {
-            var q_d = new kadi.game.Card("Q", kadi.game.Suite.DIAMONDS);
-            var q_s = new kadi.game.Card("Q", kadi.game.Suite.SPADES);
+            var q_d = diamonds("Q");
+            var q_s = spades("Q");
 
             expect(kadi.game.RuleEngine.canPlayTogetherWith(q_d, q_s)).toBe(true);
         });
@@ -112,7 +112,6 @@ describe("Card rules:", function() {
     });
 });
 
-
 describe("Move rules:", function() {
 
     var startingCard = diamonds("10");
@@ -126,6 +125,54 @@ describe("Move rules:", function() {
     it("Multiple moves are possible without grouping cards", function() {
         hand = [diamonds("5"), diamonds("6"), spades("K"), spades("A")];
         expect(kadi.game.RuleEngine.possibleMoves(diamonds("10"),hand).length).toBe(3);
+    });
+
+    describe("Group rules", function() {
+
+        it("A group is a series of cards that an be played together", function() {
+            var g = [clubs("6"), diamonds("6")];
+            expect(kadi.game.RuleEngine.evaluateGroup(g)).toBe(true);
+
+            g = [clubs("7"), clubs("8")];
+            expect(kadi.game.RuleEngine.evaluateGroup(g)).toBe(false);
+
+            g = [clubs("Q"), clubs("7")];
+            expect(kadi.game.RuleEngine.evaluateGroup(g)).toBe(true);
+        });
+
+        it("Any non playable cards in a set makes the group invalid", function() {
+            var g = [clubs("Q"), clubs("7"), diamonds("8")];
+            expect(kadi.game.RuleEngine.evaluateGroup(g)).toBe(false);
+        });
+
+        it("All consecutive cards must be playable for the cards to form a group", function() {
+            var g = [clubs("Q"), clubs("7"), diamonds("7")];
+            expect(kadi.game.RuleEngine.evaluateGroup(g)).toBe(true);
+        });
+
+        it("A group requires at least 2 cards in a hand", function() {
+            var h = [clubs("3")];
+            expect(kadi.game.RuleEngine.group(h, null).length).toBe(0);
+        });
+
+        it("A group does not exist if no cards in a hand can be played together even if they can follow each other", function () {
+            var h = [diamonds("5"), diamonds("6")];
+            expect(kadi.game.RuleEngine.group(h, null).length).toBe(0);
+        });
+
+        it("A group exists if more than one cards in a hand can be played together", function () {
+            var h = [spades("4"), clubs("4"), hearts("3"), spades("5")];
+            var tC = spades("6");
+            expect(kadi.game.RuleEngine.group(h,tC).length).toBe(1);
+        });
+
+        it("A group with more cards is more valuable than one with less", function() {
+            var h = [spades("4"), clubs("4"), hearts("4"), spades("5"), hearts("5")];
+            expect(kadi.game.RuleEngine.group(h,spades("6")).length).toBe(2);
+
+            var group = kadi.game.RuleEngine.bestGroup(h,spades("6"));
+            expect(group.length).toBe(3);
+        });
     });
 });
 
--- public/javascripts/game/rules.js
+++ public/javascripts/game/rules.js
@@ -1,7 +1,6 @@
 window.kadi.game = (function(me, $, undefined){
 
     me.Move = JS.Class({
-
         construct: function(cards) {
             this.cards = cards;
         },
@@ -54,6 +53,70 @@ window.kadi.game = (function(me, $, undefined){
                     }
                 });
                 return moves;
+            },
+
+            evaluateGroup: function(hand) {
+                if (hand.length < 2)
+                    return false;
+
+                var _result = false;
+                var _preceding = _.first(hand);
+
+                var _idx = _.indexOf(hand, _preceding, false) + 1;
+                while(_idx < hand.length) {
+                    var _following = _.first(_.rest(hand, _idx));
+                    _result = kadi.game.RuleEngine.canPlayTogetherWith(_preceding, _following);
+
+                    if (!_result)
+                        break;
+                    _idx++;
+                    _preceding = _following;
+                }
+                return _result;
+            },
+
+            canJoinGroup: function(group, card) {
+                if (!card.isAce()) {
+                    var last = _.last(group);
+                    return me.RuleEngine.canPlayTogetherWith(card,last);
+                }
+                else {
+                    return _.last(group).isAce(); //can only join groups with fellow aces
+                }
+            },
+
+            bestGroup:  function(hand, topCard) {
+                var _groups = me.RuleEngine.group(hand, topCard);
+                var _sorted = _.sortBy(_groups, function(group) {
+                    return group.length;
+                });
+                return _.last(_sorted);
+            },
+
+            group:function (hand, topCard) {
+                var _groups = [];
+                if (hand.length >= 2 && topCard != null) {
+                    var moves = kadi.game.RuleEngine.possibleMoves(topCard, hand);
+                    if (moves.length > 0) {
+                        _.each(moves, function (move, idx) {
+                            var _c = move.first();
+                            var _gp = [_c];
+
+                            _.each(hand, function (card, idx) {
+                                if (!card.eq(_c)) {
+                                    //can the card join the group
+                                    if (me.RuleEngine.canJoinGroup(_gp, card)) {
+                                        //then add it to the group
+                                        _gp.push(card);
+                                    }
+                                }
+                            });
+                            if (me.RuleEngine.evaluateGroup(_gp))
+                                _groups.push(_gp);
+                        });
+                    }
+                }
+                return _groups;
             }
         },
 
