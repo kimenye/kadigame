045e470c0f4acd55d3a0a8e60d52d1ce0411a98d
Trevor Kimenye <kimenye@gmail.com>
2012-10-12 17:33:14 +0300

    Fixed updating for durations of updates

--- public/javascripts/app/application.js
+++ public/javascripts/app/application.js
@@ -1,9 +1,23 @@
 window.kadi.app = (function(me, $, undefined){
 
-    me.Invite = JS.Class({
-        construct: function(from,at) {
+    me.Update = JS.Class({
+        construct: function(message,when) {
+            var self = this;
+            this.when = when;
+            this.lastUpdated = ko.observable(new Date());
+            this.message = message;
+            this.at = ko.computed(function() {
+                var dt = self.lastUpdated();
+                console.log("Last updated ", moment(self.when).fromNow());
+                return moment(self.when).fromNow();
+            });
+        }
+    });
+
+    me.Invite = me.Update.extend({
+        construct: function(from,when) {
+            this.parent.construct.apply(this, [from.name + " wants to play KADI!", when]);
             this.from = from;
-            this.at = moment(at).fromNow();
         }
     });
 
@@ -11,6 +25,8 @@ window.kadi.app = (function(me, $, undefined){
         construct: function(fbAccessToken, fbId, playerName) {
             var self = this;
             this.numOnline = ko.observable(1);
+            this.debug = true;
+            this.debug = false;
 //            this.me = new kadi.game.GamePlayerUI({id: fbId, name: playerName, live: true});
             this.game = new kadi.game.MultiPlayerGame({id: fbId, name: playerName, live: true});
             this.me = this.game.me;
@@ -19,6 +35,7 @@ window.kadi.app = (function(me, $, undefined){
             this.inGame = ko.observable(false);
 //            this.players.push(this.me);
             this.invites = ko.observableArray([]);
+            this.updates = ko.observableArray([]);
             this.numInvites = ko.computed(function() {
                 return self.invites().length;
             });
@@ -51,12 +68,19 @@ window.kadi.app = (function(me, $, undefined){
             SHOTGUN.listen(kadi.game.Events.INVITE_RECEIVED, function(fromId,when) {
                 var from = _.detect(self.players(), function(p) { return p.id == fromId });
 
-                self.invites.push(new me.Invite(from, when));
-                $('#online').collapse('hide');
-                $('#invites').collapse('show');
+                var invite = new me.Invite(from, when);
+                if (self.debug) {
+                    self.acceptInvite(invite);
+                }
+                else {
+                    self.invites.push(invite);
+                    $('#online').collapse('hide');
+                    $('#invites').collapse('show');
+                }
             });
 
             SHOTGUN.listen(kadi.game.Events.INVITE_ACCEPTED, function(fromId, when) {
+                $('.invite').hide();
                 var invitedPlayer = _.detect(self.players(), function(p) { return p.id == fromId });
                 self.game.master();
                 self.game.sitPlayer(invitedPlayer);
@@ -68,6 +92,7 @@ window.kadi.app = (function(me, $, undefined){
 
             this.acceptInvite = function(invite) {
                 self.invites([]); //remove all invites
+                $('.invite').hide();
                 self.me.acceptInvite(invite.from.id);
                 self.game.slave();
                 self.game.sitPlayer(invite.from);
@@ -79,6 +104,19 @@ window.kadi.app = (function(me, $, undefined){
                 }));
             }
             $('#sidebar').show();
+
+            setInterval(function() {
+                // Do something every 5 seconds
+                self.updateDurations();
+            }, 60000);
+        },
+        updateDurations: function() {
+            _.each(this.invites(), function(invite) {
+                invite.lastUpdated(new Date());
+            });
+            _.each(this.updates(), function(update) {
+                update.lastUpdated(new Date());
+            });
         },
         startGame: function() {
             $('.btn-start-game').button('loading');
