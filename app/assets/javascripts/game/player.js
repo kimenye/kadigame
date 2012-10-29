c30f3289eca3470643e50cb28416fdfddadc0f80
Trevor Kimenye <kimenye@gmail.com>
2012-10-29 14:49:57 +0300

    Added add and remove cards methods from the deck

--- app/assets/javascripts/game/player.js
+++ app/assets/javascripts/game/player.js
@@ -45,6 +45,17 @@ window.kadi = (function(me, $, undefined){
     me.Deck = JS.Class({
         construct : function() {
             this.cards = [];
+        },
+        isEmpty: function() {
+            return !kadi.isSomethingMeaningful(this.cards) || this.cards.length == 0;
+        },
+        addCard: function(card) {
+            this.cards.push(card);
+        },
+        removeCard: function(card) {
+            this.cards = _.reject(this.cards, function(c) {
+                return c.eq(card);
+            });
         }
     });
 
